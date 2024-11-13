e8459468cf86bfcb0f55488ea53e2776bed7e16c
Dimetrodon-bit <mlgoodtech@outlook.com>
2024-11-13 12:29:16 -0500

    Update main_listener.php

--- dimetrodon/hidememberlist/event/main_listener.php
+++ dimetrodon/hidememberlist/event/main_listener.php
@@ -57,7 +57,8 @@ class main_listener implements EventSubscriberInterface
 		$location = $this->user->page['page'];
 
 		// Checking to see if the setting is enabled and that we are viewing a page pertaining to the memberlist.
-		if ($this->config['dimetrodon_hidememberlist_options'] && str_contains($location, 'Members'))
+		// if ($this->config['dimetrodon_hidememberlist_options'] && str_contains($location, 'Members'))
+		substr($this->user->page['page_name'], 0, strpos($this->user->page['page_name'], '.')) === 'memberlist'
 		{
 			//Load the language file. We only have to do this once now. 
 			$this->language->add_lang('common', 'dimetrodon/hidememberlist');
