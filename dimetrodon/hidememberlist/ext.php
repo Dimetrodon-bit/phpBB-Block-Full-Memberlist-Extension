0ef75702a6bf5bc92bf6f1c8d76138c77a6ade68
Dimetrodon-bit <mlgoodtech@outlook.com>
2025-06-28 13:44:59 -0400

    Update ext.php
    Starting to add php version checks.

--- dimetrodon/hidememberlist/ext.php
+++ dimetrodon/hidememberlist/ext.php
@@ -18,4 +18,21 @@ namespace dimetrodon\hidememberlist;
  */
 class ext extends \phpbb\extension\base
 {
+  	public function is_enableable()
+	{
+		$language= $this->container->get('language');
+		$language->add_lang('common', 'dimetrodon/hidememberlist');
+
+		/** 
+      * Extension can only be executed in PHP 8.1 or higher. 
+      * Check PHP version to ensure compatability.
+    */
+		if (version_compare(PHP_VERSION, '8.1.0', '<'))
+		{
+      // Display error message and do not enable if php version is below 8.1
+			return $language->lang('PHP_VERSION_TOO_LOW');
+		}
+		
+		return true;
+	}
 }
