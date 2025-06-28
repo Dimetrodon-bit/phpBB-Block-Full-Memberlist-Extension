f9553f49412f348d7a8f1fb459fd459aa7814e9c
Dimetrodon-bit <mlgoodtech@outlook.com>
2025-06-28 12:44:23 -0400

    Add files via upload
    Added a default ext.php. Will add needed PHP version checks to it later.

---
+++ dimetrodon/hidememberlist/ext.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * Hide Memberlist. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2024, [Dimetrodon]
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace dimetrodon\hidememberlist;
+
+/**
+ * Hide Memberlist Extension base
+ *
+ * It is recommended to remove this file from
+ * an extension if it is not going to be used.
+ */
+class ext extends \phpbb\extension\base
+{
+}
